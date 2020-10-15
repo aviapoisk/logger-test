9368ba2652ae5223f3fa5c220b8a9ac62645cc0c
alx <alex@aviapoisk.com>
2020-10-16 00:45:39 +0600

    add monolog handlers

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -1,5 +1,13 @@
 <?php
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\ElasticSearchHandler;
+use Monolog\Handler\MongoDBHandler;
+use Monolog\Handler\RavenHandler;
+use Monolog\Handler\RedisHandler;
+use Sentry\Laravel\SentryHandler;
+use Sentry\State\Hub;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -12,5 +20,37 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $logger = request()->query('v');
+
+    switch($logger)
+    {
+        // case 'elastic':
+        //     $handler = new ElasticsearchHandler(
+        //         \Elastica\Client(),
+        //         [
+        //             'index' => 'elastic_index_name',
+        //             'type'  => 'elastic_doc_type',
+        //         ]);
+        // break;
+
+        case 'mongo':
+            $handler = new MongoDBHandler(new \Mongo("mongodb://mongo:27017"), "logs", "prod");
+        break;
+
+        // case 'sentry':
+        //     $handler = new SentryHandler(Hub::getCurrent());
+        // break;
+
+        case 'redis':
+            $handler = new RedisHandler(new Predis\Client("tcp://redis:6379"), "logs", "prod");
+        break;
+
+        default:
+            $logger  = 'file';
+            $handler = new StreamHandler(storage_path('logs/laravel.log'));
+    }
+
+    Log::getMonolog()->pushHandler($handler)->error('Log message', ['context' => 'Hello World']);
+
+    return view('welcome', ['logger' => $logger]);
 });
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">write logs to <b>{{ $logger }}</b></div>
             </div>
         </div>
     </body>
